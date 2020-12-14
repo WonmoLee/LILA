189dce75a8a9e80d0f7268300a86344bc252661c
wonmoLee <leewonmo94@gmail.com>
2020-12-14 21:17:45 +0900

    WebServer / footer 화면설계 완료

--- LILA-Android/lila/app/src/main/java/com/gathering/lila/MainActivity.kt
+++ LILA-Android/lila/app/src/main/java/com/gathering/lila/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.gathering.lila
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tvHello.text = "방가방가"
     }
 }
